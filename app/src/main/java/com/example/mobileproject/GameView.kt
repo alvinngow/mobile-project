e0293134a9f39cdd35190d77fbf6ea90a255c03e
unknown <andy.pee.2020@scis.smu.edu.sg>
2023-10-17 20:48:19 +0800

    removed pipes

--- app/src/main/java/com/example/mobileproject/GameView.kt
+++ app/src/main/java/com/example/mobileproject/GameView.kt
@@ -13,30 +13,10 @@ class GameView : View {
     private var bird: Bird? = null
     private var handler: Handler? = Handler()
     private var runnable: Runnable? = null
-    private var arrPipes = ArrayList<Pipe>()
-    private var sumPipe = 0
-    private var distance = 0
     constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
         initBird()
-        initPipe()
         runnable = Runnable {
             invalidate()
-        }
-    }
-
-    private fun initPipe() {
-        sumPipe = 6
-        distance = 300 * Constants.SCREEN_WIDTH / 1920
-        for (i in 0 until sumPipe) {
-            if (i < sumPipe/2) {
-                arrPipes.add(Pipe(Constants.SCREEN_WIDTH + i * (Constants.SCREEN_WIDTH + 200 * Constants.SCREEN_WIDTH/1080)/ (sumPipe/2), 0f, 200*Constants.SCREEN_WIDTH/1080, Constants.SCREEN_HEIGHT/2))
-                arrPipes[arrPipes.size - 1].setBm(BitmapFactory.decodeResource(resources, R.drawable.pipe2))
-                arrPipes[arrPipes.size - 1].randomY()
-            } else {
-                // adjust y of pipe2 to end at bottom of page
-                arrPipes.add(Pipe(Constants.SCREEN_WIDTH + (i - sumPipe/2) * (Constants.SCREEN_WIDTH + 200 * Constants.SCREEN_WIDTH/1080)/ (sumPipe/2), arrPipes[i - sumPipe/2].y + arrPipes[i - sumPipe/2].height + distance + 500, 200*Constants.SCREEN_WIDTH/1080, Constants.SCREEN_HEIGHT/2))
-                arrPipes[arrPipes.size - 1].setBm(BitmapFactory.decodeResource(resources, R.drawable.pipe1))
-            }
         }
     }
 
@@ -56,17 +36,6 @@ class GameView : View {
     override fun draw(canvas: Canvas) {
         super.draw(canvas)
         bird!!.draw(canvas)
-        for (i in 0 until sumPipe) {
-            if (arrPipes[i].x < -arrPipes[i].width) {
-                arrPipes[i].x = (Constants.SCREEN_WIDTH).toFloat()
-                if (i < sumPipe/2) {
-                    arrPipes[i].randomY()
-                } else {
-                    arrPipes[i].y = arrPipes[i - sumPipe/2].y + arrPipes[i].height + distance + 300
-                }
-            }
-            arrPipes[i].draw(canvas)
-        }
         handler!!.postDelayed(runnable!!, 1000/60)
     }
 

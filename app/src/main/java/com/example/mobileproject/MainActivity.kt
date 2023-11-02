5054bd953569c92e331cf692ac81addbc5ed18e7
maars202 <simonslee332@gmail.com>
2023-11-02 21:34:05 +0800

    sorted scores

--- app/src/main/java/com/example/mobileproject/MainActivity.kt
+++ app/src/main/java/com/example/mobileproject/MainActivity.kt
@@ -77,33 +77,21 @@ class MainActivity : Activity() {
         txt_score_over = findViewById(R.id.txt_score_over)
         r1_game_over = findViewById(R.id.rl_game_over)
         gv = findViewById(R.id.gv)
+        btn_start = findViewById(R.id.btn_start)
 //        gridView = findViewById(R.id.gridView)
 //        gridView.visibility = TextView.INVISIBLE
 //        showLeaderboard()
-            btn_start = findViewById(R.id.btn_start)
         btn_start.setOnClickListener() {
             gv.start = true
             r1_game_over.visibility = RelativeLayout.INVISIBLE
             txt_score.visibility = TextView.VISIBLE
             txt_score.text = "0"
-//            score += 1;
-//            txt_score.text = score.toString()
-            txt_score.text = "Score: " + gv.score.toString()
-//            Toast.makeText(this, "score: " + score.toString(), Toast.LENGTH_SHORT).show()
-
             btn_start.visibility = Button.INVISIBLE
         }
         r1_game_over.setOnClickListener() {
             btn_start.visibility = Button.VISIBLE
             r1_game_over.visibility = RelativeLayout.INVISIBLE
             txt_score.visibility = TextView.VISIBLE
-//            gridView.visibility = TextView.VISIBLE
-//            println("showLeaderboard2")
-////            showLeaderboard()
-//            println("showLeaderboard3")
-//            score += 1;
-            txt_score.text = gv.score.toString()
-            Toast.makeText(this, "score2: " + gv.score.toString(), Toast.LENGTH_SHORT).show()
             gv.start = false
             gv.reset()
         }
